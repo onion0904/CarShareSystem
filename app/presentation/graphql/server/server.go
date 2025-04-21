596b9168bb05a6fd5ca89298f45a5356d2c1c4b2
onion0904 <takerudaze0904@gmain.com>
2025-04-22 02:26:42 +0900

    fix

--- app/presentation/graphql/server/server.go
+++ app/presentation/graphql/server/server.go
@@ -8,7 +8,6 @@ import (
 	mail_Service "github.com/onion0904/app/infrastructure/mail"
 	usecase_mail "github.com/onion0904/app/usecase/mail"
 	mymiddleware "github.com/onion0904/app/middleware"
-	// "github.com/onion0904/go-pkg/jwt"
 	
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -17,68 +16,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/vektah/gqlparser/v2/ast"
 	"github.com/onion0904/app/presentation/graphql/graph"
-	// "github.com/labstack/echo/v4"
-	// "github.com/labstack/echo/v4/middleware"
-	// echojwt "github.com/labstack/echo-jwt/v4"
 )
-
-// func Start() {
-// 	cfg := config.GetConfig()
-// 	DB := db.NewMainDB(cfg.DB)
-
-// 	Port := cfg.Server.Port
-
-// 	// Echoのインスタンスを作成
-// 	e := echo.New()
-
-// 	// ミドルウェアの設定
-// 	e.Use(middleware.Logger())
-// 	e.Use(middleware.Recover())
-	
-// 	schema := graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{DB: DB}})
-// 	if schema == nil {
-// 		log.Fatal("スキーマ生成に失敗しました")
-// 	}
-// 	graphqlHandler := handler.New(schema)
-
-
-// 	publicGroup := e.Group("/public")
-// 	privateGroup := e.Group("/private")
-
-// 	// Recover ミドルウェア適用
-// 	publicGroup.Use(mymiddleware.RecoverMiddleware)
-// 	privateGroup.Use(mymiddleware.RecoverMiddleware)
-
-// 	playgroundHandler := playground.Handler("GraphQL", "/query")
-
-// 	publicGroup.POST("/query", func(c echo.Context) error {
-// 		graphqlHandler.ServeHTTP(c.Response(), c.Request())
-// 		return nil
-// 	})
-
-// 	publicGroup.GET("/playground", func(c echo.Context) error {
-// 		playgroundHandler.ServeHTTP(c.Response(), c.Request())
-// 		return nil
-// 	})
-
-// 	secret := cfg.JWT.Secret
-// 	jwtSecret := []byte(secret)
-// 	echojwt_config := jwt.EchoJwtInit(jwtSecret)
-// 	privateGroup.Use(echojwt.WithConfig(*echojwt_config))
-
-// 	// GraphQLでは通常クエリのルートは統一して、認証の有無でアクセスを制御する
-// 	privateGroup.POST("/query", func(c echo.Context) error {
-// 		graphqlHandler.ServeHTTP(c.Response(), c.Request())
-// 		return nil
-// 	})
-
-// 	privateGroup.GET("/playground", func(c echo.Context) error {
-// 		playgroundHandler.ServeHTTP(c.Response(), c.Request())
-// 		return nil
-// 	})
-
-// 	e.Logger.Fatal(e.Start(":" + Port))
-// }
 
 func Start() {
 	cfg := config.GetConfig()
